d6455e4f330afd6464f96c15bab77850412366b9
toufikul-islam <toufikul.cse@gmail.com>
2024-11-07 13:18:40 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+  formsObj: any ={
+    name:'' ,
+    email: '',
+    subject: '',
+    message: ''
+  }
+
+  formValue: any;
+
+  onSubmit(){
+    debugger;
+    this.formValue=this.formsObj;
+  }
 }
